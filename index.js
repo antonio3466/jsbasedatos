0e9345d81a8a93240227c553a5669823d879259b
Antoniosantacruz <jorgevera8459@gmail.com>
2025-07-18 21:13:11 -0300

    Añadir cambios a index.js, notacionjs.js y index.html

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const express = require('express');
-//const sql = require('mssql');
+const sql = require('mssql');
 const app = express();
 app.use(express.json());
 
-const config = {
-    user: ' GRUPO01',
-    password: 'Grupo01',
-    server: '10.120.3.250\SQLEXPRESS',
-    database: 'tu_base_de_datos',
-    options: {
-      encrypt: false,
-      trustServerCertificate: true
-    }
-};
-  
 
+
+
+const config = {
+ user: 'GRUPO01',
+ password: 'Grupo01',
+ server: '10.120.3.250',
+ database: 'PUMA',
+ options: {
+   encrypt: false,
+   trustServerCertificate: true
+ }
+};
 
 
 
@@ -23,22 +24,23 @@ const config = {
 
 app.use(express.static('public'));
 
+
 //app.use(cors()); // Permite todas las solicitudes desde cualquier origen
 
 
-//app.get('/api/datos', async (req, res) => {
-    try {
-      await sql.connect(config);
-      const result = await sql.query('SELECT * FROM tu_tabla');
-      res.json(result.recordset);
-    } catch (err) {
-      res.status(500).send(err.message);
-    }
-   }) ;
-  
+app.get('/api/datos', async (req, res) => {
+ try {
+   await sql.connect(config);
+   const result = await sql.query('SELECT * FROM BUCOS');
+   res.json(result.recordset);
+ } catch (err) {
+   res.status(500).send(err.message);
+ }
+});
 
 
 
-app.listen(3000, () => console.log('Servidor corriendo en puerto 3000')); 
 
 
+
+app.listen(3000, () => console.log('Servidor corriendo en puerto 3000'));
