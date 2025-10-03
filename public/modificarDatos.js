1f47dabf6ebc9b462d1e45c5209fd27628c89e03
Antoniosantacruz <jorgevera8459@gmail.com>
2025-10-02 21:34:16 -0300

    cambios

--- public/modificarDatos.js
+++ public/modificarDatos.js
@@ -1,3 +1,4 @@
+
 console.log('llegue a modificar datos');
 
 const parametros = new URLSearchParams(window.location.search);
@@ -5,10 +6,51 @@ const parametros = new URLSearchParams(window.location.search);
 const id = parametros.get("parametros")
 
 console.log(id);
-document.getElementById('variable').innerText = id;
 
-document.getElementById("Apellido").value = "santacruz";
-document.getElementById("Nombre").value = "antony";
-document.getElementById("dni").value = "73848595";
-document.getElementById("FechaNacimiento").value = "1/23/32";
-document.getElementById("email").value = "gmail.com";
+const url = 'http://localhost:4000/api/personasid/'+id;
+
+console.log(url);
+fetch(url)
+  .then(response => {
+
+    if (!response.ok)  {
+
+        throw new Error (`Error enla red: ${response.statusText} ` );
+    }
+    return response.json();
+
+})
+
+.then(data =>  {
+    if (data.length > 0){
+
+
+        const personalbar = data [0] ;
+        const PersonaID = personalbar.PersonaID;
+        const Nombre = personalbar.Nombre;
+        const Apellido = personalbar.Apellido;
+        const DNI = personalbar.DNI;
+        const Email = personalbar.Email;
+        const FechaNacimiento = personalbar.FechaNacimiento;
+        
+
+        
+    document.getElementById("Apellido").value = Apellido;
+    document.getElementById("Nombre").value = Nombre;
+    document.getElementById("dni").value = DNI;
+    document.getElementById("FechaNacimiento").value = FechaNacimiento;
+    document.getElementById("email").value = Email;
+    
+
+
+   } else {
+    console.log ('No se encontraron datos de la persona.');
+}})
+.catch(error => {
+    console.error('Hubo un problema con la solicitud fetch:', error);
+});
+
+
+
+
+
